c1045299ed4978c4759ae9a7b4ca82e79559bc2e
Nathan Lord <lord.59@osu.edu>
2014-07-22 19:25:40 -0400

    Update vdts script

--- vdts.py
+++ vdts.py
@@ -8,15 +8,18 @@ Created on Sun Jul 20 16:18:42 2014
 # vdts generator pikes peak
 
 import math
+from scipy.interpolate import UnivariateSpline
 
-max_lat = 3.0
-max_decell = 6.0
+max_lat = 9.0
+max_decell = 9.0
 
-keys, dict = file_to_variables('C:/Users/Nathan/Desktop/random python scripts/pikespeakradius.csv')
-keys, zach = file_to_variables('C:/Users/Nathan/Desktop/Zach_FullRace_062914_edited_seanscript.csv')
+keys, dict = file_to_variables('C:/Users/Nathan/Desktop/random python scripts/disttoradius.csv')
+#keys, zach = file_to_variables('C:/Users/Nathan/Desktop/Zach_FullRace_062914_edited_seanscript.csv')
 
 zspeed = zach['GPS_Speed']
 zdist = zach['Distance'] * 1000
+zlat = zach['GPS_Latitude']
+zlong = zach['GPS_Longitude']
 dist = dict['dist']
 radius = dict['radius']
 
@@ -24,9 +27,15 @@ cdts = ones(size(dist)) * float('inf')
 lat = ones(size(dist)) * float('inf')
 a = ones(size(dist)) * float('inf')
 
-d_lookahead = zeros(size(dist))
 
 rdts = sqrt(max_lat*radius)
+
+#smoothcurve = UnivariateSpline(range(size(rdts)),1/rdts, s = .5)
+
+#curve = smoothcurve(range(size(rdts)))
+curve = 1/rdts
+curve[curve < 0] = 0
+rdts = 1/curve
 
 size_rdts = size(rdts)
 #size_rdts = 1000
@@ -36,7 +45,7 @@ range_rdts = range(size_rdts-1,-1,-1)
 # i  skip to inf
 # j iterate look ahead
 i = size_rdts-1
-while rdts[i] == float('inf'):
+while rdts[i] == float('inf') or rdts[i] == 0:
     i = i-1
 
 cdts[i] = rdts[i]
@@ -50,10 +59,6 @@ for k in range(i,0,-1):
         a[k] = 0
     else:
         a[k] = math.sin(math.acos(lat[k]/max_lat))*max_decell
-        if lat[k] > 0:
-            a[k] = 0
-        
-        
     
     d = dist[k] - dist[k-1]
     cdts[k-1] = math.sqrt(a[k]*2*d+cdts[k]**2)
@@ -61,9 +66,17 @@ for k in range(i,0,-1):
     if rdts[k-1] < cdts[k-1]:
         cdts[k-1] = rdts[k-1]
 
+
 figure('cdts')
 plot(dist,cdts/.44704, 'b')
 plot(zdist,zspeed, 'r')
- 
+
+figure('gps cdts')
+scatter(zlat[:1],zlong[:1], c = (cdts[:1]/.44704), edgecolors = 'none', vmin = 0, vmax = 100)
+colorbar()
+
+figure('gps zach')
+scatter(zlat,zlong, c = zspeed, edgecolors = 'none', vmin = 0, vmax = 100)
+colorbar()
 
     
